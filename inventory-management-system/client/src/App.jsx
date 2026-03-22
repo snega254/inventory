f5678467e4d9257998de8ad10d07019b64ff5c54
Snega254 <snegak.23aim@kongu.edu>
2026-03-23 05:17:57 +0530

    Remove PrivateRoute dependency, define it in App.jsx

--- inventory-management-system/client/src/App.jsx
+++ inventory-management-system/client/src/App.jsx
@@ -1,6 +1,5 @@
 ﻿import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
 import Layout from './components/Layout';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
